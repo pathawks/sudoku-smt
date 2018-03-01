16f8ff1091f9b2d211ec0c86a62e7dd65a66eb8e
Pat Hawks <pat@pathawks.com>
2018-02-28 19:14:41 -0600

    Compare BV Int UFFS

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
 txt-files   := $(wildcard 9x9/*.txt)
 smt-files   := $(foreach file,$(txt-files),$(patsubst %.txt,%.smt,$(file)))
 txt16-files := $(wildcard 16x16/*.txt)
-smt16-files := $(foreach file,$(txt16-files),$(patsubst %.txt,%int.smt,$(file))) $(foreach file,$(txt16-files),$(patsubst %.txt,%bv.smt,$(file)))
+smt16-files := $(foreach file,$(txt16-files),$(patsubst %.txt,%int.smt,$(file))) $(foreach file,$(txt16-files),$(patsubst %.txt,%bv.smt,$(file))) $(foreach file,$(txt16-files),$(patsubst %.txt,%uffs.smt,$(file)))
 zip-flags   := -9DX
 
 clean:
 	rm -f $(smt-files) $(smt16-files) 9x9.zip 16x16.zip all.zip sudoku-smt sudoku16-smt
 
-sudoku16-smt: sudoku16-smt.c
+sudoku16-int: sudoku16-int.c
 	$(CC) $< $(CFLAGS) -o $@
 
 sudoku16-bv: sudoku16-bv.c
+	$(CC) $< $(CFLAGS) -o $@
+
+sudoku16-uffs: sudoku16-uffs.c
 	$(CC) $< $(CFLAGS) -o $@
 
 sudoku-smt: sudoku-smt.c
@@ -19,11 +22,14 @@ sudoku-smt: sudoku-smt.c
 9x9/%.smt: 9x9/%.txt sudoku-smt
 	./sudoku-smt < $< > $@
 
-16x16/%int.smt: 16x16/%.txt sudoku16-smt
-	./sudoku16-smt < $< > $@
+16x16/%int.smt: 16x16/%.txt sudoku16-int
+	./sudoku16-int < $< > $@
 
 16x16/%bv.smt: 16x16/%.txt sudoku16-bv
 	./sudoku16-bv < $< > $@
+
+16x16/%uffs.smt: 16x16/%.txt sudoku16-uffs
+	./sudoku16-uffs < $< > $@
 
 all-smt: $(smt-files) $(smt16-files)
 
---
+++ sudoku16-int.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <ctype.h>
+
+const char *const smtHeader =
+    "(set-info :smt-lib-version 2.6)\n"
+    "(set-logic QF_LIA)\n"
+    "(set-info :status sat)\n"
+    "\0";
+
+const int height = 16;
+const int width  = 16;
+int produceModels = 0;
+
+void footer(void);
+
+int isNumber(char c) {
+    return (0 <= c && c <= 15);
+}
+
+int isValid(char c) {
+    return isNumber(c) || (c == '_'-'A') || (c == '.'-'0');
+}
+
+int main(void) {
+    int board[height][width];
+    for (int x=0; x<width; ++x) {
+        for (int y=0; y<height; ++y) {
+            do {
+                int c;
+                c = getchar();
+                if (c >= 'A') {
+                    c = tolower(c) - 'a' + 10;
+                } else {
+                    c -= '0';
+                }
+                board[y][x] = c;
+            } while (!isValid(board[y][x]));
+        }
+    }
+
+    fputs(smtHeader, stdout);
+    if (produceModels) {
+        puts("(set-option :produce-models true)");
+    }
+
+    for (int x=0; x<width; ++x) {
+        for (int y=0; y<height; ++y) {
+            char xpos = x < 10 ? x + '0' : x + 'A' - 10;
+            char ypos = y < 10 ? y + '0' : y + 'A' - 10;
+            if (isNumber(board[y][x])) {
+                printf("(declare-const board%c%c Int)(assert (= %d board%c%c))\n", xpos,ypos,board[y][x],xpos,ypos);
+            } else {
+                printf("(declare-const board%c%c Int)(assert (and (<= 0 board%c%c)(< board%c%c 16)))\n", xpos,ypos,xpos,ypos,xpos,ypos);
+            }
+        }
+    }
+    footer();
+}
+
+void footer(void) {
+    for (int x=0; x<16; ++x) {
+        fputs("(assert (distinct", stdout);
+        for (int y=0; y<16; ++y) {
+            printf(" board%X%X", x, y);
+        }
+        fputs("))\n", stdout);
+    }
+    for (int x=0; x<16; ++x) {
+        fputs("(assert (distinct", stdout);
+        for (int y=0; y<16; ++y) {
+            printf(" board%X%X", y, x);
+        }
+        fputs("))\n", stdout);
+    }
+    for (int xx=0; xx<16; xx+=4)
+        for (int yy=0; yy<16; yy+=4) {
+            fputs("(assert (distinct", stdout);
+            for (int x=xx; x<xx+4; ++x)
+                for (int y=yy; y<yy+4; ++y) {
+                    printf(" board%X%X", y, x);
+                }
+            fputs("))\n", stdout);
+        }
+    puts("(check-sat)");
+    if (produceModels) {
+        for (int x=0; x<16; ++x) {
+            fputs("(get-value (", stdout);
+            for (int y=0; y<16; ++y) {
+                if (y) fputs(" ", stdout);
+                printf("board%X%X", x, y);
+            }
+            fputs("))\n", stdout);
+        }
+    }
+    puts("(exit)");
+}
